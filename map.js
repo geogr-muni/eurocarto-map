92d44691d906a316cd75f475697503fd9d03f358
DajanaSnopkova <dajana.snopkova@geogr.muni.cz>
2026-04-08 16:17:35 +0200

    update transportLayer vizualization

--- map.js
+++ map.js
@@ -51,21 +51,24 @@ var hotels = [
 // Data transport
 var transport = [
     { name: "Hlavní nádraží (train)", lat: 49.1905822, lon: 16.6128025 },
-    { name: "Hlavní nádraží (tram)", lat: 49.1910819, lon: 16.6119806, details: "-> Konečného náměstí, Nerudova: 12" },
-    { name: "Konečného náměstí (trolleybus)", lat: 49.2050475, lon: 16.5958253, details: "-> Mendlovo náměstí: 25, 26" },
-    { name: "Konečného náměstí (tram)", lat: 49.2039908, lon: 16.5960158, details: "-> Hlavní nádraží: 12" },
-    { name: "Nerudova (tram)", lat: 49.2060050, lon: 16.5950717, details: "-> Hlavní nádraží: 12" },
-    { name: "Mendlovo náměstí (trolleybus)", lat: 49.1898614, lon: 16.5931872, details: "-> Konečného náměstí: 25, 26" },
+    // { name: "Hlavní nádraží (tram)", lat: 49.1910819, lon: 16.6119806, details: "-> Konečného náměstí, Nerudova: 12" },
+    // { name: "Konečného náměstí (trolleybus)", lat: 49.2050475, lon: 16.5958253, details: "-> Mendlovo náměstí: 25, 26" },
+    // { name: "Konečného náměstí (tram)", lat: 49.2039908, lon: 16.5960158, details: "-> Hlavní nádraží: 12" },
+    // { name: "Nerudova (tram)", lat: 49.2060050, lon: 16.5950717, details: "-> Hlavní nádraží: 12" },
+    // { name: "Mendlovo náměstí (trolleybus)", lat: 49.1898614, lon: 16.5931872, details: "-> Konečného náměstí: 25, 26" },
     { name: "Grand Hotel (bus long-distance)", lat: 49.1935158, lon: 16.6144772 },
     { name: "Zvonařka (bus long-distance)", lat: 49.1858217, lon: 16.6167317 },
-]
+];
+
+var stopsUrl = "https://raw.githubusercontent.com/geogr-muni/eurocarto-map/refs/heads/main/zastavky2.geojson";
+var linesUrl = "https://raw.githubusercontent.com/geogr-muni/eurocarto-map/refs/heads/main/linky.geojson";
 
 // Data sightseeing
 var sightseeing = [
     { name: "Špilberk castle", lat: 49.1944442, lon: 16.5993594, url: "https://podzemibrno.cz/en/mista/hrad-spilberk/" },
     { name: "Ossuary at St. James' church", lat: 49.1963336, lon: 16.6080114, url: "https://podzemibrno.cz/en/mista/kostnice-u-sv-jakuba/" },
     { name: "Water tanks", lat: 49.1957067, lon: 16.5913517, url: "https://podzemibrno.cz/en/mista/vodojemy-zluty-kopec/" },
-]
+];
 
 // Data restaurants
 var restaurants = [
@@ -83,7 +86,7 @@ var restaurants = [
     { name: "Pivovarská Mendlák s.r.o.", lat: 49.1909661, lon: 16.5927697, url: "https://www.pivovarska-starobrno.cz", desc: "Czech classics" },
     { name: "Bonjour Vietnam", lat: 49.1884711, lon: 16.5976431, url: "www.facebook.com/Bonjour-Vietnam" },
     { name: "Ramen Brno", lat: 49.1890425, lon: 16.5955769, url: "https://ramen-brno.cz/", desc: "Best Ramen in Brno, Open 11-14 and 17-20" },
-]
+];
 
 // Data pubs
 var pubs = [
@@ -92,7 +95,7 @@ var pubs = [
     { name: "U všech svatých", lat: 49.1887736, lon: 16.5975906, url: "www.facebook.com/uvsechsvatych", desc: "Try the cherry liqueur with chili" },
     { name: "Immigrant", lat: 49.2031617, lon: 16.5968136, url: "https://www.theimmigrant.cz/", desc: "When you feel like having a Guinness" },
     { name: "Na dobré cestě", lat: 49.2028956, lon: 16.5984131, url: "http://jsmenadobreceste.cz/", desc: "Pub with atmosphere" },
-]
+];
 
 //Data cafe
 var cafes = [
@@ -100,7 +103,7 @@ var cafes = [
     { name: "MARINĀDA", lat: 49.2010383, lon: 16.5961556, url: "http://www.marinada-store.cz/" },
     { name: "Punkt.", lat: 49.2061967, lon: 16.6019803, url: "https://www.facebook.com/" },
     { name: "Kafec", lat: 49.2035439, lon: 16.5935281, url: "https://www.kafec.cz/" },
-]
+];
 
 function createAwesomeMarker(icon, markerColor) {
     return L.AwesomeMarkers.icon({
@@ -136,6 +139,58 @@ transport.forEach(function (node) {
         .bindPopup(buildContent(node), { className: 'transparent-popup', closeButton: false })
         .addTo(transportLayer);
 });
+
+function getLinesColor(property) {
+    switch (property) {
+        case 'T12': return '#558e90';
+        case 'TB26': return '#426E70';
+        case 'TB25': return '#426E70';
+        default: return '#ff7f00';
+    }
+};
+
+// 2. Transport (Lines)
+fetch(linesUrl)
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: function (feature) {
+                return {
+                    color: getLinesColor(feature.properties.naz_linky),
+                    weight: 2
+                };
+            }
+        }).addTo(transportLayer);
+    });
+
+function getStopColor(property) {
+    switch (property) {
+        case '12': return '#558e90';
+        case '25/26': return '#426E70';
+        default: return '#ff7f00';
+    }
+};
+
+// 2. Transport (Stops)
+fetch(stopsUrl)
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 6,
+                    fillColor: getStopColor(feature.properties.linka_c),
+                    color: "#ffffff",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(`<b>Stop: ${feature.properties.name}</b><br>Line: ${feature.properties.linka_c}`, { className: 'transparent-popup', closeButton: false });
+            }
+        }).addTo(transportLayer); // Add to the SAME group
+    });
 
 // 3. Sightseeing (Popups)
 sightseeing.forEach(function (landmark) {
